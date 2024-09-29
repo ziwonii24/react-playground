3c19ef9716e28389e98d8e56240629de195e2475
ziwonii24 <ziwonii25@gmail.com>
2024-09-29 18:22:42 +0900

    :construction: ADD router frame

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Error!</div>,
+    children: [
+      { index: true, element: <div>index</div> },
+      { path: "test1", element: <div>test1</div> },
+      { path: "test2", element: <div>test2</div> },
+      { path: "test3", element: <div>test3</div> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
